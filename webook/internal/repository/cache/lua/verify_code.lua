bd51fcfeaa5debeeacde9b8e173509761b1ea141
liupch <2556309411@qq.com>
2023-10-05 21:48:30 +0800

    短信验证码登录（二）改

--- webook/internal/repository/cache/lua/verify_code.lua
+++ webook/internal/repository/cache/lua/verify_code.lua
@@ -9,8 +9,12 @@ if cnt <= 0 then
     return -1
 elseif code == expectedCode then
     -- 输入正确
+    -- 用完，不能再用了
+    redis.call("set", cntKey, -1)
     return 0
 else
     -- 用户手一抖输错了
+    -- 可验证次数减一
+    redis.call("decr", cntKey)
     return -2
 end
